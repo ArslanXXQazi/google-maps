5e80218b9cee91b1560c2d39b864a3f79d903b60
Arslan Qazi <arslanqazi1999@gmail.com>
2025-03-08 12:46:57 +0500

    Update location.dart

--- google_maps/lib/src/location/location.dart
+++ google_maps/lib/src/location/location.dart
@@ -106,6 +106,10 @@ class _LocationState extends State<Location> {
       ),
       body: Column(children: [
         
+        Text(
+            "Place : $placeName"
+        )
+        
       ],),
     );
   }
