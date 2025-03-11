afa2b301dfcf9de28252ac7c5f7e6d8fd49bfcf8
Arslan Qazi <arslanqazi1999@gmail.com>
2025-03-11 12:58:07 +0500

    Update location.dart

--- google_maps/lib/src/location/location.dart
+++ google_maps/lib/src/location/location.dart
@@ -107,6 +107,7 @@ class _LocationState extends State<Location> {
       ),
        body: lat==null || long==null? Center(child: Text('Fetching Current Location'),):
        GoogleMap(
+         onMapCreated: completer(),
          initialCameraPosition: CameraPosition(
              target: LatLng(lat!, long!),
             zoom: 15,
