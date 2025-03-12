140b21688590a48544028e16ab0f5ad0b634930b
Arslan Qazi <arslanqazi1999@gmail.com>
2025-03-12 12:11:40 +0500

    Update user_current_location.dart

--- google_maps/lib/src/location/user_current_location.dart
+++ google_maps/lib/src/location/user_current_location.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class UserCurrentLocation extends StatefulWidget {
+  const UserCurrentLocation({super.key});
+
+  @override
+  State<UserCurrentLocation> createState() => _UserCurrentLocationState();
+}
+
+class _UserCurrentLocationState extends State<UserCurrentLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
