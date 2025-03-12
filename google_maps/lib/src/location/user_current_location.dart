7f51f42042f639906df46d628f1486dc0d6a1a62
Arslan Qazi <arslanqazi1999@gmail.com>
2025-03-12 12:24:15 +0500

    Update user_current_location.dart

--- google_maps/lib/src/location/user_current_location.dart
+++ google_maps/lib/src/location/user_current_location.dart
@@ -10,14 +10,16 @@ class UserCurrentLocation extends StatefulWidget {
 
 class _UserCurrentLocationState extends State<UserCurrentLocation> {
   @override
-
+  GoogleMapController? googleMapController;
+  Set<Marker> _marker={};
   static CameraPosition _initialCameraPosition=CameraPosition(target: LatLng(33.6844, 73.0479),zoom: 14);
 
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.location_on),),
       body: GoogleMap(
-          initialCameraPosition: _initialCameraPosition
+          initialCameraPosition: _initialCameraPosition,
+          markers: _marker,
       ),
 
     );
