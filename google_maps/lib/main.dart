6d56d93600dc25ba96e6773172284e55f6b4a463
Arslan Qazi <arslanqazi1999@gmail.com>
2025-03-18 21:21:59 +0500

    update

--- google_maps/lib/main.dart
+++ google_maps/lib/main.dart
@@ -18,8 +18,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: UserCurrentLocation()
-      //Location(),
+      home: //UserCurrentLocation()
+      Location(),
     );
   }
 }
