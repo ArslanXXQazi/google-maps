873e60f73ae0917a5d7f9abb4b056742cad9e7d9
Arslan Qazi <arslanqazi1999@gmail.com>
2025-03-18 21:51:26 +0500

    Create custom_markers.dart

---
+++ google_maps/lib/src/location/custom_markers.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomMarkers extends StatefulWidget {
+  const CustomMarkers({super.key});
+
+  @override
+  State<CustomMarkers> createState() => _CustomMarkersState();
+}
+
+class _CustomMarkersState extends State<CustomMarkers> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
